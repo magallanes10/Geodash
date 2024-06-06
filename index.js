4fe96295c2e22905b415d6b54642cee997d4afbb
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-06-06 13:26:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ fs.readdirSync(routesDir).forEach(file => {
     }
 });
 
-const port = process.env.PORT || 3000;
+const port = 8080;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
